232fb238a20c0d497508e7984a2fa5db07f5b8ca
LucasSherlock <lucas.sherlocknz@gmail.com>
2020-11-19 14:46:19 +1300

    fixed path bug

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var concat = require('gulp-concat');
 gulp.task('compile', function() {
   return gulp.src(['./src/main.js','./src/util.js','./src/cmds.js','./src/entry.js'])
   .pipe(concat('extension.js'))
-  .pipe(gulp.dest('./dist/'))
+  .pipe(gulp.dest('./'))
 });
