d73cd9f0bef71e6acddf74f3a8cad48a45f7b266
tikkanz <tikkanz@gmail.com>
2020-11-20 00:24:45 +1300

    Change task name, configure as default build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-gulp.task('compile', function() {
+gulp.task('build', function() {
   return gulp.src(['./src/main.js','./src/util.js','./src/cmds.js','./src/entry.js'])
   .pipe(concat('extension.js'))
   .pipe(gulp.dest('./'))
