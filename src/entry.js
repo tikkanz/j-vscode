08c33cf4184e123a3a9e9cfd8bfe6d13b7d0fe1d
tikkanz <tikkanz@gmail.com>
2021-04-19 00:58:01 +1200

    Script formatting

--- src/entry.js
+++ src/entry.js
@@ -5,85 +5,85 @@ let csel, lsel;
 // ---------------------------------------------------------------------
 // ctrl E runs selection on one line
 function executeSelection(e) {
- if (!e.selection) return;
- getselections(e);
- let txt;
- if ((lsel[0] === lsel[1]) && (csel[0] !== csel[1]))
-  sendterm(e.document.getText(e.selection));
+  if (!e.selection) return;
+  getselections(e);
+  let txt;
+  if ((lsel[0] === lsel[1]) && (csel[0] !== csel[1]))
+    sendterm(e.document.getText(e.selection));
 }
 
 // ---------------------------------------------------------------------
 function executeLine(e) {
- runline(e, false);
+  runline(e, false);
 }
 
 // ---------------------------------------------------------------------
 function executeLineAdvance(e) {
- runline(e, true);
+  runline(e, true);
 }
 
 // ---------------------------------------------------------------------
 function getline(e, p) {
- return e.document.lineAt(p).text;
+  return e.document.lineAt(p).text;
 }
 
 // ---------------------------------------------------------------------
 function getselections(e) {
- csel = [e.selection.start.character, e.selection.end.character];
- lsel = [e.selection.start.line, e.selection.end.line];
+  csel = [e.selection.start.character, e.selection.end.character];
+  lsel = [e.selection.start.line, e.selection.end.line];
 }
 
 // ---------------------------------------------------------------------
 function movecursor(n) {
- vscode.commands.executeCommand("cursorMove", {
-  to: "down",
-  by: "wrappedLine",
-  value: n
- });
- vscode.commands.executeCommand("cursorMove", {
-  to: "wrappedLineEnd"
- });
+  vscode.commands.executeCommand("cursorMove", {
+    to: "down",
+    by: "wrappedLine",
+    value: n
+  });
+  vscode.commands.executeCommand("cursorMove", {
+    to: "wrappedLineEnd"
+  });
 }
 
 // ---------------------------------------------------------------------
 // returns next non-empty line, entry from given position
 function readentry(e) {
- let res = readentry1(e);
- let p = res[0];
- while (p < e.document.lineCount) {
-  let s = getline(e, p);
-  if (s.trim().length) break;
-  p++;
- }
- return [p, res[1]];
+  let res = readentry1(e);
+  let p = res[0];
+  while (p < e.document.lineCount) {
+    let s = getline(e, p);
+    if (s.trim().length) break;
+    p++;
+  }
+  return [p, res[1]];
 }
 
 // ---------------------------------------------------------------------
 function readentry1(e) {
- let p = lsel[0];
- let r = getline(e, p++);
- if (r.trim().length === 0 || !ismultiline(r)) return [p, r];
- while (p < e.document.lineCount) {
-  let s = getline(e, p++);
-  r += "\n" + s;
-  if (s === ")") break;
- }
- return [p, r];
+  let p = lsel[0];
+  let r = getline(e, p++);
+  if (r.trim().length === 0 || !ismultiline(r)) return [p, r];
+  while (p < e.document.lineCount) {
+    let s = getline(e, p++);
+    r += "\n" + s;
+    if (s === ")") break;
+  }
+  return [p, r];
 };
 
 // ---------------------------------------------------------------------
 function runline(e, advance) {
- if (!e.selection) return;
- getselections(e);
- let res = readentry(e);
- let pos = res[0];
- let txt = res[1];
- sendterm(txt);
- if (advance)
-  movecursor(pos - lsel[0]);
+  if (!e.selection) return;
+  getselections(e);
+  let res = readentry(e);
+  let pos = res[0];
+  let txt = res[1];
+  sendterm(txt);
+  if (advance)
+    movecursor(pos - lsel[0]);
 }
 
 // ---------------------------------------------------------------------
 function sendterm(txt) {
- terminal.sendText('\u0015' + txt, !txt.endsWith('\n'));
+  terminal.sendText('\u0015' + txt, !txt.endsWith('\n'));
 }
